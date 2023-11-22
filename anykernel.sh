d6ebe608dd3cc912f5763a810a20c05d1223e581
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2023-11-22 12:10:35 +0530

    AnyKernel3

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=KernelSU by Rushi Ranpise
+kernel.string=AnyKernel3 by Rushi Ranpise
 do.devicecheck=1
 do.modules=0
 do.systemless=1
